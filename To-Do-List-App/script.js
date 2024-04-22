30d962b1f49c61dfb7d32df2c79273f4a54875a8
Alex Milenkov <alexmilenkov07@gmail.com>
2024-04-22 22:58:14 +0300

    Finished the to-do app
    Now it has functionality that allows the user to save his tasks on the local browser, and can also delete all the tasks, and also he can delete a specific task.

--- To-Do-List-App/script.js
+++ To-Do-List-App/script.js
@@ -1,29 +1,69 @@
+// Declare all needed the info
 const addTaskButton = document.querySelector('.row button')
 const taskElement = document.getElementById('task-input')
 const tasksListElement = document.querySelector('.list-container')
-
-console.log(tasksListElement);
+const clearAllTaskElement = document.querySelector('header p')
 
 addTaskButton.addEventListener('click', () => {
     const task = taskElement.value 
 
+    // Check if the user do not enter input
     if (!task) {
         return
     } 
 
+    // Create the task 
     const liElement = document.createElement('li')
     liElement.textContent = task
 
+    // Make a close element that can remove the task
+    const closeElement = document.createElement('img')
+    closeElement.src = 'images/x-button.png'
+
+    closeElement.addEventListener('click', () => {
+        liElement.remove()
+
+        saveData()
+    })
+
+    liElement.append(closeElement)
+
     tasksListElement.appendChild(liElement)
 
-    taskElement.value = ''
-
+    // Add functionality to check and ucheck specific task
     liElement.addEventListener('click', () => {
         if (!liElement.classList.contains('checked-item')) {
             liElement.classList.add('checked-item')
         } else {
             liElement.classList.remove('checked-item')
         }
+
+        saveData()
     })
+
+    // Reset the input
+    taskElement.value = ''
+
+    saveData()
 })
 
+clearAllTaskElement.addEventListener('click', () => {
+    if (!taskElement) {
+        return
+    }
+    tasksListElement.innerHTML = ''
+
+    saveData()
+})
+
+// This function saves the data to the local storage 
+function saveData() {
+    localStorage.setItem('data', tasksListElement.innerHTML)
+}
+
+// This function shows all the changes that were made by starting the browser
+function onLoad() {
+    tasksListElement.innerHTML = localStorage.getItem('data')
+}
+
+onLoad()
