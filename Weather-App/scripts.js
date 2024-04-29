d8e36f82171631e9fa66535057da46f2822bd275
Alex Milenkov <alexmilenkov07@gmail.com>
2024-04-29 23:28:56 +0300

    Added functionality to the app
    Need to make error handling when the user type non-existing city

--- Weather-App/scripts.js
+++ Weather-App/scripts.js
@@ -1,10 +1,83 @@
-const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=52c2a5409957192df3dfe96db224a5c9&units=metric&q=Sofia`
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=52c2a5409957192df3dfe96db224a5c9&units=metric`
 
-async function weather() {
-    const response = await fetch(baseUrl)
-    const weatherData = await response.json()
+const inputCityElement = document.getElementById('weather-input')
+const searchButton = document.querySelector('.search-area button')
 
-    console.log(weatherData);
-}
+const statusImageElement = document.querySelector('.status')
+const tempreratuteElement = document.querySelector('.temp')
+const cityElement = document.querySelector('.city')
+const humidityElement = document.querySelector('.humidity')
+const widnSpeedElement = document.querySelector('.wind')
 
-weather()
+
+searchButton.addEventListener('click', async () => {
+    if (!inputCityElement.value) {
+        return
+    }
+
+    const city = inputCityElement.value
+
+    try {
+        const response = await fetch(baseUrl + `&q=${city}`)
+        const data = await response.json()
+
+        if (!response.ok) {
+            throw new Error("This city doesn't exist...")
+        }
+
+        // Get all the info from the response
+        const status = data.weather[0].main
+        const temp = data.main.temp.toFixed(1)
+        const humidity = data.main.humidity
+        const windSpeed = data.wind.speed
+
+        // Set the weather status image
+        const newStatusImage = document.createElement('img')
+
+        switch (status) {
+            case 'Clouds':
+                newStatusImage.src = 'images/clouds.png'
+                break;
+            case 'Drizzle':
+                newStatusImage.src = 'images/drizzle.png'
+                break;
+            case 'Mist':
+                newStatusImage.src = 'images/mist.png'
+                break;
+            case 'Rain':
+                newStatusImage.src = 'images/rain.png'
+                break;
+            case 'Snow':
+                newStatusImage.src = 'images/snow.png'
+                break;
+            case 'Clear':
+                newStatusImage.src = 'images/clear.png'
+                break;
+        }
+
+        // Set the status image
+        statusImageElement.innerHTML = '' 
+        statusImageElement.append(newStatusImage)
+
+        // Set the temperature
+        tempreratuteElement.textContent = `${temp}°C`
+
+        // Set the city
+        cityElement.textContent = city
+
+        // Set the humidity
+        humidityElement.textContent = `${humidity}%`
+
+        // Set the wind speed
+        widnSpeedElement.textContent = `${windSpeed} km/h`
+
+        inputCityElement.value = ''
+    }
+    catch (error) {
+        const errorMessage = error.message
+
+        inputCityElement.value = errorMessage
+
+        // TO DO make error message appears in div.weather and add some style to it
+    }
+})
